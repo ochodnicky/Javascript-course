0486115ccf6d0859b7db2345b953a585d10bb085
Ondřej Ochodnický <ochodnickyondrej@gmail.com>
2019-12-08 22:20:21 +0100

    How JS works

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,19 +1,105 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+// functions
+/*
+calculateAge(1992);
 
+function calculateAge(year) {
+    console.log(2019 - year);
+}
 
+//retirement(1992);
+var retirement = function(year) {
+    console.log(65 - (2019 - year));
+}
+*/
 
+// variables
+/*
+console.log(age);
+var age = 27;
 
+function foo() {
+    console.log(age);
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
+*/
 
+/*
+var a = 'Hello!';
+first();
 
+function first() {
+    var b = 'Hi!';
+    second();
 
+    function second() {
+        var c = 'Hey!';
+        console.log(a + b + c);
+    }
+}
 
+*/
 
+/*
+var a = 'Hello!';
+first();
 
+function first() {
+    var b = 'Hi!';
+    second();
 
+    function second() {
+        var c = 'Hey!';
+        third()
+    }
+}
 
+function third() {
+    var d = 'John';
+    //console.log(c);
+    console.log(a+d);
+}
 
+*/
+
+//console.log(this);
+/*
+calculateAge(1992);
+
+function calculateAge(year) {
+    console.log(2019 - year);
+    console.log(this);
+}
+*/
+var ondra = {
+    name: 'Ondřej',
+    yearOfBirth: 1992,
+    calculateAge: function() {
+        console.log(this);
+        console.log(2019 - this.yearOfBirth);
+        /*
+        function innerFunction() {
+            console.log(this);
+        }
+        innerFunction();
+        */
+    }
+}
+
+ondra.calculateAge();
+
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1990
+};
+
+mike.calculateAge = ondra.calculateAge;
+mike.calculateAge();
 
 
 
