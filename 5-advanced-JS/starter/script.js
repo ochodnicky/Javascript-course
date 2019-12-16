f3535bb4f17dd10eadb787a5a62afe9b1f20dabc
Ondřej Ochodnický <ochodnickyondrej@gmail.com>
2019-12-16 19:48:41 +0100

    advanced JS;

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -28,7 +28,6 @@ mark.calculateAge();
 console.log(john.lastName);
 console.log(jane.lastName);
 console.log(mark.lastName);
-*/
 
 // Object.create
 
@@ -48,3 +47,164 @@ var jane = Object.create(personProto, {
     yearOfBirth: { value: 1985 },
     job: { value: 'designer' },
 });
+
+
+// Primitives
+var a = 27;
+var b = a;
+
+a = 46;
+
+console.log(a);
+console.log(b);
+
+
+// Objects
+var obj1 = {
+    name: 'John',
+    age: 27
+}
+
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+// Functions
+
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'Lisbon'
+}
+
+function change(a, b) {
+    a = 30;
+    b.city = 'San Francisco';
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
+
+*/
+/*
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for(i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calcAge(el) {
+    return 2016 - el;
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+function maxHeartRate(el) {
+    if (el >= 18 && el <= 81) {
+        return Math.round(206.9 - (0.67 * el));
+    } else {
+        return -1;
+    }
+}
+
+var ages = arrayCalc(years, calcAge);
+var fullAges = arrayCalc(years, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
+
+console.log(ages);
+console.log(fullAges);
+console.log(rates);
+*/
+
+/*
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function(name) {
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log('What subject do you teach, ' + name + '?');
+        }
+    } else {
+        return function(name) {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+teacherQuestion('Ondřej');
+designerQuestion('Ondřej');
+designerQuestion('Jane');
+designerQuestion('Mark');
+designerQuestion('Mike');
+*/
+
+/*
+function game() {
+    var score = Math.random() * 10;
+    console.log(score >= 5);
+}
+game(); */
+
+/* CLOSURES 
+(function() {
+    var score = Math.random() * 10;
+    console.log(score >= 5);
+})();
+
+//console.log(score);
+
+(function(goodLuck) {
+    var score = Math.random() * 10;
+    console.log(score >= 5 - goodLuck);
+})(5);
+*/
+
+/*
+function retirement(retirementAge) {
+    return function(yearOfBirth) {
+        var a = ' years left until retirement.';
+        var age = 2016 - yearOfBirth;
+        console.log((retirementAge - age) + a);
+    }
+}
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementGermany(1990);
+retirementUS(1990);
+retirementIceland(1990); 
+
+//retirement(66)(1990);
+
+*/
+
+/* Challenge */
+function interviewQuestion(job) {
+    return function(name) {
+        if (job === 'designer') {
+            console.log(name + ', can you please explain what UX design is?');
+        } else if (job === 'teacher') {
+            console.log('What subject do you teach, ' + name + '?');
+        } else {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+
+interviewQuestion('teacher')('Ondřej');
+interviewQuestion('designer')('Ondřej');
